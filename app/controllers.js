ab23f191b49fa63d30473216479cbb618d10af8d
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2014-09-21 19:29:51 +0000

    Up to the "adding a empty songs message"

---
+++ app/controllers.js
@@ -0,0 +1,14 @@
+angular.module("myApp.controllers", []).controller("songCtrl", function($scope) {
+    $scope.songs = [];
+                    
+    $scope.newSong = { };
+
+    $scope.addSong = function(artist, title) {
+        $scope.songs.push({
+            artist: artist,
+            title: title
+        });
+        $scope.newSong.artist = "";
+        $scope.newSong.title = "";
+    }
+})
